dab59dddfc1904c20301810fff57cd660f669c65
M_Ishikawa <ishikawam@nifty.com>
2014-12-09 22:25:52 +0900

    update phantomjs-screenshot

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,39 @@
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-phantomjs-screenshot');
-    grunt.registerTask('default', 'phantomjs_screenshot');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('default', ['clean', 'phantomjs_screenshot']);
+
     grunt.initConfig({
+        clean: {
+            capture: {
+                src: ['capture/'],
+            },
+        },
+
         phantomjs_screenshot: {
             options: {
                 viewport: '1024x768',
                 server: 'http://komasui.boobie.jp/',
             },
-            mockup: {
+            pc: {
                 expand: true,
                 cwd: 'www/',
                 src: ['**/*.html'],
-                dest: 'capture/',
+                dest: 'capture/pc/',
+                ext: '.png'
+            },
+            mobile: {
+                options: {
+                    viewport: '320x568',
+                },
+                expand: true,
+                cwd: 'www/',
+                src: ['**/*.html'],
+                dest: 'capture/mobile/',
                 ext: '.png'
             },
         },
+
     });
 };
